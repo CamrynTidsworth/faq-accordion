3ccf7d03e58d7d74124571f6fc3c1623dfa114fa
Camryn Tidsworth <camitidsworth@gmail.com>
2024-11-01 16:45:57 -0400

    javascript complete

--- script.js
+++ script.js
@@ -3,22 +3,53 @@
 const accordions = document.querySelectorAll('.accordion');
 accordions.forEach(accordion => {
     accordion.addEventListener('click', e => {
-        let accordionBtn = accordion.querySelector('button');
+        //let accordionBtn = accordion.querySelector('button');
         accordion.classList.toggle('active');
-        if (accordionBtn.textContent === '-') {
-            accordionBtn.textContent = '+';
-        } else {
-            accordionBtn.textContent = '-';
-        }
+        //if (accordionBtn.textContent === '-') {
+        //    accordionBtn.textContent = '+';
+        //} else {
+        //  accordionBtn.textContent = '-';
+        //}
     })
 })
 
-function imageFun() {
-    var Image_Id = document.getElementById('plus-icon');
-    if (Image_Id.src.match("icon-plus.svg")) {
-        Image_Id.src = "icon-minus.svg";
+
+function changeIcon1() {
+    var Image_Id = document.getElementById('minus-icon-1');
+    if (Image_Id.src.match("assets/images/icon-plus.svg")) {
+        Image_Id.src = "assets/images/icon-minus.svg";
     }
     else {
-        Image_Id.src = "icon-plus.svg";
+        Image_Id.src = "assets/images/icon-plus.svg";
+    }
+}
+
+function changeIcon2() {
+    var Image_Id = document.getElementById('plus-icon-2');
+    if (Image_Id.src.match("assets/images/icon-plus.svg")) {
+        Image_Id.src = "assets/images/icon-minus.svg";
+    }
+    else {
+        Image_Id.src = "assets/images/icon-plus.svg";
+    }
+}
+
+function changeIcon3() {
+    var Image_Id = document.getElementById('plus-icon-3');
+    if (Image_Id.src.match("assets/images/icon-plus.svg")) {
+        Image_Id.src = "assets/images/icon-minus.svg";
+    }
+    else {
+        Image_Id.src = "assets/images/icon-plus.svg";
+    }
+}
+
+function changeIcon4() {
+    var Image_Id = document.getElementById('plus-icon-4');
+    if (Image_Id.src.match("assets/images/icon-plus.svg")) {
+        Image_Id.src = "assets/images/icon-minus.svg";
+    }
+    else {
+        Image_Id.src = "assets/images/icon-plus.svg";
     }
 }
